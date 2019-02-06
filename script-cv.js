e58b9ec8ddf0d95ff4c52eea4f932b61cfe124da
p1816619 <46898864+p1816619@users.noreply.github.com>
2019-02-06 23:22:57 +0200

    Update script-cv.js

--- script-cv.js
+++ script-cv.js
@@ -1,11 +1,45 @@
-var x = 0;
+/*
 
 window.addEventListener("scroll", function() { //ajoute une fonction efectuée lorsque l'utilisateur scroll sur la page
-deplacement=window.scrollY - (window.innerHeight-document.body.offsetHeight);
+deplacement=(window.scrollY - (window.innerHeight-document.body.offsetHeight))/window.innerHeight;
 // Calculez le déplacement ici ...
 
 document.getElementById('progress_bar').value = deplacement; // Modifie la valeur de la barre de progression pour qu'elle corresponde au déplacement du scroll
 });
+*/
+
+window.addEventListener("scroll", function() { //ajoute une fonction efectuée lorsque l'utilisateur scroll sur la page
+let deplacement = ((window.scrollY)*100)/(document.body.offsetHeight-window.innerHeight);
+// Calculez le déplacement ici ...
+
+document.getElementById('progress_bar').value = deplacement; // Modifie la valeur de la barre de progression pour qu'elle corresponde au déplacement du scroll
+});
+
+
+
+/*
+function myScroll() { // Définition de la fonction de scroll
+    var progress = document.getElementById('progress_bar').value; // Valeur de la barre de progression
+      var scrollPercent =(window.scrollY/(scrollHeight - windowHeight))*100;
+
+    window.scroll(0, value); // Déplacement de la page de 0 px latéralement et 'value' px horizontalement
+    console.log(0,value);
+ }
+ 
+ setInterval(function(){ myScroll(); },40);
+
+
+
+
+/*
+function myScroll() { // Définition de la fonction de scroll
+    var progress = document.getElementById('progress_bar').value; // Valeur de la barre de progression
+      var scrollPercent =(scrollTop/(scrollHeight - windowHeight))*100;
+
+    window.scroll(0, value); // Déplacement de la page de 0 px latéralement et 'value' px horizontalement
+    console.log(0,value);
+ }
+
 
 
 //function myFunction() {
@@ -13,4 +47,4 @@ document.getElementById('progress_bar').value = deplacement; // Modifie la valeu
  
 /*window.scrollY // Valeur d u scroll en pixel	
 document.body.offsetHeight // Hauteur totale de la page en pixel
-window.innerHeight // Hauteur de l'écran en pixel	*/					
+window.innerHeight // Hauteur de l'écran en pixel	*/
